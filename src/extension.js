da2d772452a4c37379413fb5efa1d74f2d0b3a92
jackhuang <2504448153@qq.com>
2024-05-20 11:59:30 +0800

    feat: 行尾语言诊断提示

--- src/extension.js
+++ src/extension.js
@@ -45,9 +45,63 @@ function provideHover(document, position) {
     return;
 }
 
+const errorDecorationType = vscode.window.createTextEditorDecorationType({
+    after: { margin: '0 0 0 1rem', textDecoration: 'none' },
+});
+
+// 行尾语言诊断提示
+function updateDiagnostics(editor) {
+    const active = vscode.workspace.getConfiguration('eslint-rules-zh-plugin').get('enableErrorLens', true);
+    if (!active) {
+        editor.setDecorations(errorDecorationType, []);
+        return;
+    }
+    // TODO 用配置文件自定义颜色
+    const getColorForSeverity = (severity) => {
+        switch (severity) {
+            case vscode.DiagnosticSeverity.Error:
+                return 'red';
+            case vscode.DiagnosticSeverity.Warning:
+                return 'yellow';
+            case vscode.DiagnosticSeverity.Information:
+                return 'blue';
+            case vscode.DiagnosticSeverity.Hint:
+                return 'green';
+            default:
+                return 'white';
+        }
+    }
+    const diagnostics = vscode.languages.getDiagnostics(editor.document.uri);
+    const decorationsArray = [];
+    diagnostics
+        .filter((diagnostic) => diagnostic.source === 'eslint' && typeof diagnostic.code === 'object')
+        .forEach((diagnostic) => {
+            const line = diagnostic.range.end.line;
+            const lineText = editor.document.lineAt(line);
+            const endPos = lineText.range.end;
+            const ruleId = String(diagnostic.code.value);
+            const rule = eslintRules[ruleId];
+            if(!rule || !rule.zh) return;
+            let ruleText = /typescript-eslint/.test(ruleId)
+                ? `ts-eslint提示：${rule.zh}`
+                : `eslint提示：${rule.zh}`;
+            const decoration = {
+                range: new vscode.Range(endPos, endPos),
+                renderOptions: {
+                    after: {
+                        contentText: `${ruleText}`,
+                        color: getColorForSeverity(diagnostic.severity),
+                    },
+                },
+            };
+            decorationsArray.push(decoration);
+        });
+
+    editor.setDecorations(errorDecorationType, decorationsArray);
+}
+
 // 激活插件
 function activate(context) {
-
     const selector = [];
     for (const language of ['javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'vue']) {
         selector.push({ language, scheme: 'file' });
@@ -58,6 +112,24 @@ function activate(context) {
     context.subscriptions.push(vscode.languages.registerHoverProvider(selector, {
         provideHover
     }));
+
+    vscode.languages.onDidChangeDiagnostics(() => {
+        const editor = vscode.window.activeTextEditor;
+        if (editor) updateDiagnostics(editor)
+    });
+    vscode.window.onDidChangeActiveTextEditor((editor) => {
+        if (editor) updateDiagnostics(editor);
+    });
+    vscode.workspace.onDidChangeTextDocument((event) => {
+        const editor = vscode.window.activeTextEditor;
+        if (editor && editor.document.uri === event.document.uri) updateDiagnostics(editor)
+    });
+    vscode.workspace.onDidChangeConfiguration(e => {
+        // 监听配置启用/禁用
+        if(e.affectsConfiguration('eslint-rules-zh-plugin.enableErrorLens')) {
+            vscode.window.visibleTextEditors.forEach(editor => updateDiagnostics(editor));
+        }
+    })
 }
 
 module.exports = {
